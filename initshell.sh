d9b9bf68d2b1df36a2068083b5e41da9f52f6a53
Jian Meng <jianmeng@me.com>
2012-09-30 18:03:37 +0800

    init vim part

---
+++ initshell.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+echo "call pathogen#infect()
+syntax on
+filetype plugin indent on" > ~/.vimrc
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git clone git://github.com/oscarh/vimerl.git
+git clone git://github.com/myusuf3/numbers.vim.git
+git clone git://github.com/myusuf3/numbers.vim.git
