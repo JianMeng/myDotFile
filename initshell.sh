f80291ce693d75d05761c5496860916f9f32445f
Jian Meng <jianmeng@me.com>
2012-09-30 18:11:02 +0800

    fix one mistaken

--- initshell.sh
+++ initshell.sh
@@ -7,4 +7,4 @@ filetype plugin indent on" > ~/.vimrc
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git clone git://github.com/oscarh/vimerl.git
 git clone git://github.com/myusuf3/numbers.vim.git
-git clone git://github.com/myusuf3/numbers.vim.git
+git clone git://github.com/kchmck/vim-coffee-script.git
