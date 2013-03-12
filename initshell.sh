77a896edeb3a2a8abbec23423ebd79a7a9cd8ef1
Jian Meng <JianMeng@me.com>
2013-03-12 14:42:36 +0800

    Update initshell.sh

--- initshell.sh
+++ initshell.sh
@@ -3,7 +3,16 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 echo "call pathogen#infect()
 syntax on
-filetype plugin indent on" > ~/.vimrc
+filetype plugin indent on
+".vimrc
+map <c-f> :call JsBeautify()<cr>
+" or
+autocmd FileType javascript noremap <buffer>  <c-f> :call JsBeautify()<cr>
+" for html
+autocmd FileType html noremap <buffer> <c-f> :call HtmlBeautify()<cr>
+" for css or scss
+autocmd FileType css noremap <buffer> <c-f> :call CSSBeautify()<cr>
+" > ~/.vimrc
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git clone git://github.com/oscarh/vimerl.git
 git clone git://github.com/myusuf3/numbers.vim.git
